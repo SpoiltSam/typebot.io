aa6b89f00eaf1b98bceaef1cea089ca999ce3f67
Sam <118583656+SpoiltSam@users.noreply.github.com>
2025-05-31 16:21:16 +1000

    Update migrate-deploy.ts

--- packages/prisma/scripts/migrate-deploy.ts
+++ packages/prisma/scripts/migrate-deploy.ts
@@ -1,4 +1,4 @@
 import { executePrismaCommand } from "./executeCommand";
-
-if (process.env.DATABASE_URL?.startsWith("postgres"))
+//Try swapping DATABASE_URL to DIRECT_URL
+if (process.env.DIRECT_URL?.startsWith("postgres"))
   executePrismaCommand("prisma migrate deploy");
